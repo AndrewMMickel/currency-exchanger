cee9867bebebfbe1d329ab08ccc97f536cf2ed72
AndrewMMickel <andrew.m.mickel@gmail.com>
2020-09-16 10:45:18 -0700

    started backend logic

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
 import $ from 'jquery';
 import 'bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+
+$(document).ready(function () {
+  $(":submit").click(function (event) {
+      
+  });
+});
