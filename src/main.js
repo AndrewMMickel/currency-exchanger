fcaf10d3ec0fc999fb9931c354715e51272cb83d
AndrewMMickel <andrew.m.mickel@gmail.com>
2020-09-21 09:09:00 -0700

    fixing styles.css import

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./css/styles.css";
+import "styles.css";
 import currencyConvert from "./c-exchanger.js";
 
 let currentrate = [];
